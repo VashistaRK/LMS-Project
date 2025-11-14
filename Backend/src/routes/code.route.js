bbb5c9e74be34388996b9562ff7ddec639bb347a
yashwanth adepu <117250637+Darkunquie@users.noreply.github.com>
2025-11-14 17:24:27 +0530

    bugfix on routing

--- Backend/src/routes/code.route.js
+++ Backend/src/routes/code.route.js
@@ -6,7 +6,6 @@ import {
   submitSolution,
   createQuestion,
   updateQuestion,
-  runCode,
 } from "../controllers/coding.controller.js";
 
 const router = express.Router();
@@ -16,8 +15,6 @@ router.get("/random", getRandomQuestion);
 router.get("/:id", getQuestionById);
 router.post("/", createQuestion);
 router.put("/:id", updateQuestion);
-
-router.post("/run", runCode);
 router.post("/submit", submitSolution);
 
 export default router;
