de7f8767545aa415699041a965545d911963548f
VASHISTA RAMAN <152428514+VashistaRK@users.noreply.github.com>
2025-11-25 10:10:05 +0530

    ui updates

--- Backend/src/routes/upload.route.js
+++ Backend/src/routes/upload.route.js
@@ -1,4 +1,5 @@
 import express from "express";
+import multer from "multer";
 import path from "path";
 import fs from "fs";
 import { uploadDocumentQuestions } from "../controllers/quiz.controller.js";
